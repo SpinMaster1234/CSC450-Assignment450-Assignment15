727165d45bead63e702f0163e3204ade3a31ed8a
SpinMaster1234 <55300744+SpinMaster1234@users.noreply.github.com>
2023-04-04 21:26:52 -0500

    Add files via upload

--- CSC450Assignment15.c
+++ CSC450Assignment15.c
@@ -2,31 +2,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 int main(){
-    const char * filePath = "genesis.txt";
-    const char * copyPath = "genesis_copy.txt";
-    FILE * inputStream = fopen(filePath, "rb");
-    FILE * outputStream = fopen(copyPath, "wb");
+    char * filePath = "genesis.txt";
+    char * copyPath = "genesis_copy.txt";
+    FILE * inputStream = fopen(filePath, "rb+");
+    FILE * outputStream = fopen(copyPath, "wb+");
     int size = 0;
     fseek(inputStream, 0, SEEK_END);
     size = ftell(inputStream);
-    int end = 0;
-    end = size / 2;
+    int set2 = 0;
+    set2 = size / 2;
+    int set1=size-set2;
+    //Create Fork
     int p_id = fork();
+    //For second half of the file
     if (p_id == 0){
         fseek(inputStream, 0, SEEK_SET);
-        char buffer[end];
-        fread(buffer, end, 1, inputStream);
-        fwrite(buffer, end, 1, outputStream);
+        char buffer[set2];
+        fread(buffer, set2, 1, inputStream);
+        fwrite(buffer, set2, 1, outputStream);
         fflush(outputStream);
         fclose(inputStream);
         fclose(outputStream);
-    } else {    
-        fseek(inputStream, end + 1, SEEK_SET);
-        char buffer[size - end];
-        fread(buffer, (size-end), 1, inputStream);
-        fseek(outputStream, end + 1, SEEK_SET);
+       //For first half of file
+    } 
+    else {    
+        fseek(inputStream, set2 + 1, SEEK_SET);
+        char buffer[set1];
+        fread(buffer, (set1), 1, inputStream);
+        fseek(outputStream, set2 + 1, SEEK_SET);
         sleep(1);
-        fwrite(buffer, (size-end), 1, outputStream);
+        fwrite(buffer, (set1), 1, outputStream);
         fflush(outputStream);
         fclose(inputStream);
         fclose(outputStream);
